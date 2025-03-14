2ecea43f87048d25d926bc4d13205e934b08df86
Felipe Mora <felipe.moraa13@gmail.com>
2025-03-13 22:23:39 -0600

    Update index.js back

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: process.env.SESSION_SECRET || 'your_secret_key',
   resave: true,
   saveUninitialized: true,
   store: new MongoStore({
